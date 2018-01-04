d32f6887a5245804a3c14c03111e386e8f5e9c4f
JoaoPedroCardoso <joaopedrocar@hotmail.com>
2018-01-04 19:39:17 -0200

    first commit

---
+++ src/main/java/br/com/challenge/zup/TestZupApplication.java
@@ -0,0 +1,12 @@
+package br.com.challenge.zup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestZupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestZupApplication.class, args);
+	}
+}
