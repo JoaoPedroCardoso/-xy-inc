db3acb124e402c1cf0bda07fb282b01b6ed813dd
JoaoPedroCardoso <joaopedrocar@hotmail.com>
2018-01-11 23:59:52 -0200

    Configuração final de testes

--- src/main/java/br/com/challenge/zup/resources/PoiResource.java
+++ src/main/java/br/com/challenge/zup/resources/PoiResource.java
@@ -4,8 +4,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
